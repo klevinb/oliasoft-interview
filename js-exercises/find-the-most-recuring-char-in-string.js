2720ddd44647f28c9d1c9ebabe500e34016f455d
klevinb <oneklevin@gmail.com>
2023-02-22 11:57:46 +0100

    Create two js exerciese

---
+++ js-exercises/find-the-most-recuring-char-in-string.js
@@ -0,0 +1,13 @@
+/**
+ * Write a function which will go through strings of an array
+ * If we have this array: ["Fredrik", "Steffen", "Mariana", "Klevin"] we want to only
+ * return an array of the characters that are recuring in a string
+ * the result of the function for this emaple of string would be this:
+ * [
+ *  { F: 1, r: 2, e: 1, d: 1, i: 1, k: 1 },
+ *  { S: 1, t: 1, e: 2, f: 2, n: 1 },
+ *  { M: 1, a: 3, r: 1, i: 1, n: 1 },
+ *  { K: 1, l: 1, e: 1, v: 1, i: 1, n: 1 }
+ * ]
+ */
+
---
+++ js-exercises/remove-duplicate-names.js
@@ -0,0 +1,14 @@
+/**
+ * Write a function which will remove the duplicate strings from an array
+ * If we have this array: ["Fredrik", "Steffen", "Fredrik", "Klevin"] we want to remove
+ * one of the strings which is equal to "Fredrik" since it is a duplicate element
+ * and the result of the function would be this: ["Fredrik", "Steffen", "Klevin"]
+ */
+
+
+// Solution as prefered
+
+
+
+// Solution with reduce()
+// WIKI for reduce: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce

9c02fa496d16ee7c40c309e48bfc9dabd9ece752
klevinb <oneklevin@gmail.com>
2023-02-22 12:01:43 +0100

    Update one of the js exercieses

---
+++ js-exercises/count-string-chars.js
@@ -0,0 +1,13 @@
+/**
+ * Write a function which will go through strings of an array
+ * If we have this array: ["Fredrik", "Steffen", "Mariana", "Klevin"] we want to return an array
+ * of object with the number of chars and how many times they occure in the string
+ * The result of the function for this example of strings would be this:
+ * [
+ *  { F: 1, r: 2, e: 1, d: 1, i: 1, k: 1 },
+ *  { S: 1, t: 1, e: 2, f: 2, n: 1 },
+ *  { M: 1, a: 3, r: 1, i: 1, n: 1 },
+ *  { K: 1, l: 1, e: 1, v: 1, i: 1, n: 1 }
+ * ]
+ */
+
